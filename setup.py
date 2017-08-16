32cd5c020abd8090ac57cafc98397948500c30cd
Diogo Munaro <diogo.munaro@corp.globo.com>
2017-08-16 15:52:35 -0300

    Bump 0.0.8

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup
 
 setup(
     name="brokerpackager",
-    version="0.0.7",
+    version="0.0.8",
     description="Package Manager based on broker messages",
     author="Big Data",
     author_email="bigdata@corp.globo.com",
     license='MIT',
+    include_package_data=True,
     install_requires=[
         "click==6.7",
         "rpy2==2.8.6",
