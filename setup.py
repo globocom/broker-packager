ab6c722827c728b5dcd104532aa48ccdd0962987
Diogo Munaro <diogo.munaro@corp.globo.com>
2017-08-16 15:40:27 -0300

    Bump 0.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="brokerpackager",
-    version="0.0.5",
+    version="0.0.6",
     description="Package Manager based on broker messages",
     author="Big Data",
     author_email="bigdata@corp.globo.com",
